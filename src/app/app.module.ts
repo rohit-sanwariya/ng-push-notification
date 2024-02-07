3e5fed5abf5aff2197eb672f2391035114492b1b
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-02-07 14:50:38 +0530

    pwa added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,13 @@ import { getMessaging, provideMessaging } from '@angular/fire/messaging';
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp({"projectId":"push-notification-dotnet","appId":"1:179158154095:web:9d870962092b058351c69a","storageBucket":"push-notification-dotnet.appspot.com","apiKey":"AIzaSyDScEFfXl68KSzpqQAbBMpVq90KmfEi9No","authDomain":"push-notification-dotnet.firebaseapp.com","messagingSenderId":"179158154095","measurementId":"G-X89H96GVD2"})),
-    provideMessaging(() => getMessaging())
+    provideMessaging(() => getMessaging()),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
