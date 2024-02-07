df976403a5ab71dcd102d9bc2cd94a301617bd0b
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-02-07 14:52:36 +0530

    env generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,18 @@ import { ServiceWorkerModule } from '@angular/service-worker';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp({"projectId":"push-notification-dotnet","appId":"1:179158154095:web:9d870962092b058351c69a","storageBucket":"push-notification-dotnet.appspot.com","apiKey":"AIzaSyDScEFfXl68KSzpqQAbBMpVq90KmfEi9No","authDomain":"push-notification-dotnet.firebaseapp.com","messagingSenderId":"179158154095","measurementId":"G-X89H96GVD2"})),
+    provideFirebaseApp(() => initializeApp(
+      {
+        "projectId":"push-notification-dotnet",
+      "appId":"1:179158154095:web:9d870962092b058351c69a",
+      "storageBucket":"push-notification-dotnet.appspot.com",
+      "apiKey":"AIzaSyDScEFfXl68KSzpqQAbBMpVq90KmfEi9No",
+      "authDomain":"push-notification-dotnet.firebaseapp.com",
+      "messagingSenderId":"179158154095",
+      "measurementId":"G-X89H96GVD2"
+    }
+    )
+    ),      
     provideMessaging(() => getMessaging()),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase:      {
+        "projectId":"push-notification-dotnet",
+      "appId":"1:179158154095:web:9d870962092b058351c69a",
+      "storageBucket":"push-notification-dotnet.appspot.com",
+      "apiKey":"AIzaSyDScEFfXl68KSzpqQAbBMpVq90KmfEi9No",
+      "authDomain":"push-notification-dotnet.firebaseapp.com",
+      "messagingSenderId":"179158154095",
+      "measurementId":"G-X89H96GVD2"
+    }
+};
