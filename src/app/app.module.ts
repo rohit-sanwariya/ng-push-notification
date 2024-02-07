e15571237777bf9ac72cac39fae5abf6abb8885f
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-02-07 14:50:00 +0530

    angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"push-notification-dotnet","appId":"1:179158154095:web:9d870962092b058351c69a","storageBucket":"push-notification-dotnet.appspot.com","apiKey":"AIzaSyDScEFfXl68KSzpqQAbBMpVq90KmfEi9No","authDomain":"push-notification-dotnet.firebaseapp.com","messagingSenderId":"179158154095","measurementId":"G-X89H96GVD2"})),
+    provideMessaging(() => getMessaging())
   ],
   providers: [],
   bootstrap: [AppComponent]
